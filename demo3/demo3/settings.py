20ddab0c580f91fdf79f3a951463513d4efd43b2
zpf <2631287831@qq.com>
2019-06-18 19:40:32 +0800

    项目部署3

--- demo3/demo3/settings.py
+++ demo3/demo3/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
+STATIC_ROOT = '/home/zpf/newpro/newpro/demo3/staticroot'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,"static/media")
 
